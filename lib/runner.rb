8d137cfa94b0a87bfcc2d1f9cbc0a95ccde81fe6
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-05-22 11:43:30 -0600

    Move all text into a text module

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,81 +1,3 @@
-# require './lib/game_board.rb'
-# require './lib/board_positions.rb'
-# require 'pry'
-# include BasicGameFunctions
-# game = GameBoard.new
-# #player_setup_1
-# start_time = Time.now.to_i
-# puts "Place first ship (formatted like 'A1 B1')"
-# first_ship = nil
-# loop do
-#   fsp = FirstShipPlacement.new(gets.chomp)
-#   if fsp.valid?
-#     first_ship = fsp
-#     break
-#   else
-#     puts "Please choose a valid position, the coordinates must be on the grid,"+
-#       " and adjacent without wrapping the grid (how could a ship do that?)."
-#   end
-# end
-# game.place_small_ship(first_ship.first_ship)
-# puts "Your first ship has been placed!"
-# game.print_screen
-# #player_setup_2
-# puts "Place second ship (formatted like 'A1 C1',"+
-#      " must be exactly 2 spaces apart, no diagonals"+
-#      " and cannot overlap your first ship!)"
-# second_ship = nil
-# loop do
-#   ssp = SecondShipPlacement.new(gets.chomp)
-#   if ssp.valid(game.valid_options)
-#     second_ship = ssp
-#     break
-#   else
-#     puts "Please choose a valid position, the coordinates must be on the grid,"+
-#          " and in a horizontal or vertical line, without wrapping the board"+
-#          " or overlapping your first ship.  Check the map!"
-#          game.print_screen
-#   end
-# end
-# game.place_large_ship(second_ship.second_ship)
-# puts 'Both of your ships have been placed!'
-# game.print_screen
-# #computer_setup
-# computer = GameBoard.new
-# two_space = computer.two_space_positions.shuffle.first.join(' ')
-# first = FirstShipPlacement.new(two_space)
-# computer.place_small_ship(first.first_ship)
-# three_space = computer.valid_options.values.shuffle.first
-# three_space = three_space.sort.join(' ')
-# second = SecondShipPlacement.new(three_space)
-# computer.place_large_ship(second.second_ship)
-# puts 'The enemy ships are somewhere on this grid!'
-# computer.print_no_ship_screen
-# #shooting_phase
-# returns = []
-# computer_returns = []
-# loop do
-#   puts 'Commander, what are the coordinates for your shot?'
-#   response = computer.shoot(gets.chomp)
-#   if response != 0 && response != 1
-#     returns << response
-#     computer_returns << game.computer_shoot
-#     puts '=================================================='
-#     puts 'Shots taken at your ships!'
-#     game.print_screen
-#     puts 'Shots you have taken at the enemy ships!'
-#     computer.print_no_ship_screen
-#     if returns.include?(3) && returns.include?(4)
-#       end_time = Time.now.to_i
-#       congratulations(returns, start_time, end_time)
-#       break
-#     elsif computer_returns.include?(3) && computer_returns.include?(4)
-#       end_time = Time.now.to_i
-#       sorry(computer_returns, start_time, end_time)
-#       break
-#     end
-#   end
-# end
 require './lib/game_logic.rb'
 
 battleship = GameLogic.new
@@ -85,24 +7,22 @@ loop do
   play = battleship.operator(choice)
   break if play == true
 end
-puts "Place first ship (formatted like 'A1 B1')"
+puts place_first_ship
 loop do
   valid = battleship.valid_first_ship(gets.chomp)
   break if valid == true
 end
 battleship.player_setup_1
-puts "Place second ship (formatted like 'A1 C1',"+
-     " must be exactly 2 spaces apart, no diagonals"+
-     " and cannot overlap your first ship!)"
+puts place_second_ship
 loop do
   valid = battleship.valid_second_ship(gets.chomp)
   break if valid == true
 end
 battleship.player_setup_2
 battleship.computer_setup
-puts 'Commander, what are the coordinates for your shot?'
+puts coordinates_needed
 loop do
   win = battleship.shots_fired(gets.chomp)
   break if win == true
-  puts 'Commander, what are the coordinates for your shot?'
+  puts coordinates_needed
 end
---
+++ lib/text.rb
@@ -0,0 +1,80 @@
+module Text
+
+  def start_game
+    'Welcome to Battleship! Command your small fleet to victory!'+
+    "\nWould you like to (p)lay, read the (i)nstrcutions, or (q)uit?"
+  end
+
+  def place_first_ship
+    "Place first ship (formatted like 'A1 B1')"
+  end
+
+  def place_second_ship
+    "Place second ship (formatted like 'A1 C1',"+
+    ' must be exactly 2 spaces apart, no diagonals'+
+    ' and cannot overlap your first ship!)'
+  end
+
+  def invalid_play_read_quit
+    'Please choose to (p)lay, read the (i)nstructions, or (q)uit!'
+  end
+
+  def instructions
+    "Commander, we have become embroiled in a small scale skirmish!\n\n"+
+    "You must direct the two ships we have here into strategic positions!\n\n"+
+    'As you can see, one of our ships will take up two of the coordinates'+
+    " on the grid, and the other will take up three coordinates.\n\n"+
+    'We rely on you to give us the adjacent coordinates for the small'+
+    " ship and the first and last coordinates for the large ship.\n\n"+
+    'Remember, for maximum accuracy, our ships must be vertical'+
+    " or horizontal on the grid!\n\n"+
+    'Our enemy will be following the same logic when positioning their'+
+    " ships, which are the same sizes as ours.\n\n"+
+    'Commander, we need you to direct our fire at the grid to sink the'+
+    " enemy ships before we are sunk ourselves!\n\n"+
+    'Choose one coordinate on the grid at a time to fire at and we will'+
+    " notify you of the results immediately!\n\n"+
+    'Our enemy will most likely fire back while we reload, we will'+
+    " keep you notified of the coordinates they have fired on!\n\n"+
+    'Now, Commander, will you lead us? Or just walk away?...'
+  end
+
+  def first_ship_placed
+    'Your first ship has been placed!'
+  end
+
+  def both_ships_placed
+    'Both of your ships have been placed!'
+  end
+
+  def enemy_ships_appear
+    'The enemy ships are somewhere on this grid!'
+  end
+
+  def invalid_first_ship
+    'Please choose a valid position, the coordinates must be on the grid,'+
+    ' and adjacent without wrapping the grid (how could a ship do that?).'
+  end
+
+  def invalid_second_ship
+    'Please choose a valid position, the coordinates must be on the grid,'+
+    ' and in a horizontal or vertical line, without wrapping the board'+
+    ' or overlapping your first ship.  Check the map!'
+  end
+
+  def coordinates_needed
+    'Commander, what are the coordinates for your shot?'
+  end
+
+
+  def linebreak
+    '=================================================='
+  end
+
+  def shots_at_you
+    'Shots taken at your ships!'
+  end
+
+  def our_shots
+    'Shots you have taken at the enemy ships!'
+  end
